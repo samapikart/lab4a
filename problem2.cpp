ea05303e6c89dc176e0ab1ddd25a0726e3fa38df
samapikart <31254492+samapikart@users.noreply.github.com>
2017-09-14 22:55:31 +0530

    Create problem2.cpp

---
+++ problem2.cpp
@@ -0,0 +1,16 @@
+
+  /*C++ program to accept radius of a circle and find diameter, perimeter and area using functions*/
+  
+ -void circle-operations()
+ +void circle_operations()
+  { 
+    float rad=0;
+    float dia, ar, per;
+ @@ -20,6 +20,6 @@ void circle-operations()
+  }
+  int main()
+  {
+ -  circle operations();
+ +  circle_operations();
+    return 0; 
+  }
